b3cf8a89f301066b6330ee4309d12cf5db3ff242
PinkNY <skdudgns1234@kakao.com>
2024-10-25 05:48:04 +0000

    w

--- backend/config/settings/production.py
+++ backend/config/settings/production.py
@@ -3,9 +3,9 @@ from .base import *
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 # 운영 환경에서의 보안 설정
-SECURE_SSL_REDIRECT = True
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
+SECURE_SSL_REDIRECT = False
+SESSION_COOKIE_SECURE = False
+CSRF_COOKIE_SECURE = False
 
 # Azure MySQL 데이터베이스 설정 (skdudgns 사용자와 비밀번호 9P@ssw0rd 사용)
 DATABASES['default']['USER'] = 'skdudgns'

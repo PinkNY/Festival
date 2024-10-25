e1fdf9c2e4e7cc24b6583e4ce96b0c090d275f01
PinkNY <skdudgns1234@kakao.com>
2024-10-25 06:36:37 +0000

    1

--- backend/config/asgi.py
+++ backend/config/asgi.py
@@ -8,9 +8,9 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 """
 
 import os
-
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+# 운영 환경에서는 기본적으로 production 설정을 사용하고, 개발 환경에서는 development 설정 사용
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', os.getenv('DJANGO_SETTINGS_MODULE', 'config.settings.production'))
 
 application = get_asgi_application()
